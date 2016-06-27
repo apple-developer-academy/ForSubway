2119edd5c1182291e87399996f33fd27b36a0e83
Gabriel Cavalcante <gabrielsoucavalcante@gmail.com>
2016-06-27 14:19:47 -0300

    Initial Commit

---
+++ ForSubway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForSubway
+//
+//  Created by Pedro Albuquerque on 27/06/16.
+//  Copyright © 2016 Gabriel Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
