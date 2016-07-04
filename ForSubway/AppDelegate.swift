ae16389b911686d04163814c222556a3c7285f46
Gabriel Cavalcante <GabrielCavalcante@gabriels-imac.local>
2016-07-04 07:38:01 -0300

    Final

--- ForSubway/AppDelegate.swift
+++ ForSubway/AppDelegate.swift
@@ -16,12 +16,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch
-        
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
         
         return true
     }
-
+    
+    /*
+        Registrar UILocalNotification com UIUserNotificationSettings
+     */
+    
+    /*
+        Criar simples UILocalNotification
+        - fireDate
+        - alertTitle
+        - alertBody
+        - applicationIconBadgeNumber
+        - soundName
+        - userInfo
+        - alertAction
+        - category
+     */
+    
+    /*
+        Criar UIMutableUserNotificationAction para a UIMutableUserNotificationCategory/Tipo de demonstracao das Actions
+     
+        UIMutableUserNotificationAction()
+        - identifier
+        - title
+        - destructive
+        - activationMode
+        - authenticationRequired
+     
+        UIMutableUserNotificationCategory()
+        - identifier
+        - setActions([actions], context)
+     
+        Demonstrar com handleActionWithIdentifier
+     */
+    
+    enum Actions:String {
+        case cancel = "CANCEL"
+        case open = "OPEN"
+    }
+    
+    /*
+        Criar simples UIAlertController para demonstrar userInfo com didReceiveLocalNotification
+     */
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -34,7 +75,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        application.applicationIconBadgeNumber = 0
+        //application.applicationIconBadgeNumber = 0
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
